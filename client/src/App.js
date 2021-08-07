7a00d0f1f0d5d29ac7056cdd53e07d3170abd434
Arnaut <lou.arnaut@gmail.com>
2021-08-07 08:51:21 -0700

    added profile features and thought adding

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,41 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 // Integrate Apollo into the fron end of the application
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+// Retrieve the token to ensure user is online
+import { setContext } from '@apollo/client/link/context';
+
+// Integrate react router dom
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Establish a new link to the GraphQL server at its /graphql endpoint 
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+// Create a middleware function to retrieve the token and comgine it with httpLink
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 // Instantiate the Apollo Client instance and create the connection to the API endpoint
 // Also instantiate a new cache object
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
@@ -24,13 +46,22 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header/>
-        <div className='container'>
-          <Home/>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
         </div>
-        <Footer/>
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
