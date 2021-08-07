ff719482e0bf3a36a64e1c00858cefcce3fd8f2f
Arnaut <lou.arnaut@gmail.com>
2021-08-07 15:02:42 -0700

    updated server.js with code that was never mentioned in the lesson

--- server/server.js
+++ server/server.js
@@ -25,12 +25,16 @@ async function startExpressApolloServer() {
 
 startExpressApolloServer();
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
